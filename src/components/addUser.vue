c3f412f8e8fc6b67cbb312ea828f3378c942fa59
Chakshu <chakshujawa@gmail.com>
2021-09-02 14:00:51 +0530

    Added first file

---
+++ src/components/addUser.vue
@@ -0,0 +1,106 @@
+<template>
+  <div id="add-user">
+    <h2>Add a new User</h2>
+    <form>
+      <label>First Name:</label>
+      <input v-model="newUser.fname" type="text" required/>
+      <label>Last Name</label>
+      <input v-model="newUser.lname" type="text" required/>
+      <label>Age</label>
+      <input v-model="newUser.age" type="number" required />
+      <label>Gender</label>
+      <select v-model="newUser.gender">
+          <option>Male</option>
+          <option>Female</option>
+          <option>Others</option>
+          <!-- <option v-for="sex in sexes">{{sex}}</option> -->
+      </select>
+    </form>
+    <!-- <div id="preview">
+      <h3>User Preview</h3>
+      <p>First Name:{{fname}}</p>
+      <p>Last Name:{{lname}}</p>
+      <p>Age:{{age}}</p>
+      <p>Sex:{{sex}}</p>
+    </div> -->
+    <div id="button_property">
+      <button @click="storeUser" id="button">ADD</button>
+    </div>
+    
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  data() {
+    return{
+      newUser: {
+        fname:"",
+        lname:"",
+        age:"",
+        gender:""
+      }
+      
+        
+        // sexes:['Male', 'Female', 'Others']
+    }
+  },
+
+  methods: {
+    storeUser() {
+      this.$emit('userStored', this.newUser)
+      this.$emit('addClose','list')
+      this.fname=""
+      this.lname=""
+      this.age=""
+      this.gender
+    },
+    
+  }
+
+
+      
+};
+</script>
+
+
+<style>
+
+#add-user *{
+    box-sizing: border-box;
+}
+
+#add-user {
+    margin: 20px auto;
+    max-width:500px;
+}
+label {
+    display: block;
+    margin: 20px 0 10px;
+}
+input[type="text"]{
+    display: block;
+    width: 100%;
+    padding: 8px;
+}
+#preview {
+    padding: 10px 20px;
+    border: 1px dotted black;
+    margin: 20px 0;
+}
+h3 {
+    margin-top: 10px;
+}
+#button_property {
+  text-align: center;
+  margin-top: 10px;
+}
+
+#button {
+  text-align: center;
+  padding: 10px 40px;
+}
+
+</style>
---
+++ src/components/userList.vue
@@ -0,0 +1,97 @@
+<template>
+    <div id="userList">
+        <AddUser v-if="mode=='add'" @userStored="addUser($event)" @addClose="openAdd($event)" ></AddUser>
+        <div v-if="mode=='list'">
+            <h3 id="header">User List</h3>
+            <table>
+            <thead>
+                <tr>
+                <th>SNo.</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Age</th>
+                <th>Gender</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(user,index) in users" :key="index">
+                <th>{{index+1}}</th>
+                <td>{{user.fname}}</td>
+                <td>{{user.lname}}</td>
+                <td>{{user.age}}</td>
+                <td>{{user.gender}}</td>
+                <td>
+                    <form>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                    </form>
+                </td>
+                </tr>
+            </tbody>
+            </table>
+             <div id="addUser">
+                <button @click="openAdd('add')">Add User</button>
+            </div>
+        </div>
+        
+       
+
+    </div>
+</template>
+
+
+<script>
+import AddUser from "./addUser.vue";
+export default {
+    components: {
+        AddUser
+    },
+    //props:['newUser'],
+    data() {
+        return {
+            mode:'list',
+            users: [
+            {
+                fname:'Sam',
+                lname:'John',
+                age:'25',
+                gender:'Male'
+            },
+            {
+                fname:'Joe',
+                lname:'Root',
+                age:'28',
+                gender:'Male'
+            }
+        ]
+        }
+        
+    },
+    methods: {
+        openAdd(val) {
+            this.mode= val
+        },
+        addUser(user) {
+            
+            this.mode='list'
+            this.users.push({
+                fname:user.fname,
+                lname:user.lname,
+                age:user.age,
+                gender:user.gender
+            })
+            
+        }
+        
+    },   
+    mounted(){
+        this.mode='list'
+    }
+    
+}
+</script>
+
+
+<style>
+
+</style>
