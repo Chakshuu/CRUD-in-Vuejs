1801e7fc224f2bce12d91bf7b6b7a9319530cf17
Chakshu <chakshujawa@gmail.com>
2021-09-06 10:47:49 +0530

    Edit and Delete Components

--- src/components/editUser.vue
+++ src/components/editUser.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="add-user">
-    <h2 class="header">Add a new User</h2>
+  <div id="edit-user">
+    <h2 class="header">Edit the User</h2>
     <form>
       <label>First Name:</label>
       <input v-model="newUser.fname" type="text" required/>
@@ -16,15 +16,9 @@
           <!-- <option v-for="sex in sexes">{{sex}}</option> -->
       </select>
     </form>
-    <!-- <div id="preview">
-      <h3>User Preview</h3>
-      <p>First Name:{{fname}}</p>
-      <p>Last Name:{{lname}}</p>
-      <p>Age:{{age}}</p>
-      <p>Sex:{{sex}}</p>
-    </div> -->
+    
     <div class="button_property">
-      <button :disabled="isDisabled()" @click="storeUser"  class="button" >ADD</button>
+      <button :disabled="isDisabled()"  @click="edit" class="button">SUBMIT</button>
     </div>
     
   </div>
@@ -34,47 +28,41 @@
 
 <script>
 export default {
-  data() {
-    return{
-      //button_event: false,
-      newUser: {
-        fname:"",
-        lname:"",
-        age:"",
-        gender:""
-      }
-    }
-        
-        // sexes:['Male', 'Female', 'Others']
+
+    //props:['users'],
+    
+    data(){
+        return{
+            newUser:{
+                fname:"",
+                lname:"",
+                age:"",
+                gender:""
+            }
+        }
+
+    
   },
-  
-  // computed:{
-
-  // },
-
-  
 
   methods: {
-
-    isDisabled () {
-    //console.log("in disabled")
-    if(this.newUser.fname!="" && this.newUser.lname!="" && this.newUser.age!="" && this.newUser.gender!=""){
+    isDisabled() {
+      if(this.newUser.fname!="" && this.newUser.lname!="" && this.newUser.age!="" && this.newUser.gender!=""){
         return false;
     }
     else {
       return true;
     }
-    
-  },
-    
-    storeUser() {
-       
-      this.$emit('userStored', this.newUser)
-      this.$emit('addClose','list')
-      this.fname=""
-      this.lname=""
-      this.age=""
-      this.gender
+
+    },
+    edit() {
+        this.$emit('userEdited', this.newUser)
+        //this.users.$set(index,{fname:this.newUser.fname,lname:this.newUser.lname,age:this.newUser.age,gender:this.newUser.gender})
+        //this.$emit('userStored', this.newUser)
+        this.$emit('addClose','list')
+        this.fname=""
+        this.lname=""
+        this.age=""
+        this.gender=""
     },
     
   }
@@ -87,11 +75,11 @@ export default {
 
 <style>
 
-.add-user *{
+#edit-user *{
     box-sizing: border-box;
 }
 
-.add-user {
+#edit-user {
     margin: 20px auto;
     max-width:500px;
 }
@@ -99,12 +87,16 @@ label {
     display: block;
     margin: 20px 0 10px;
 }
-input,select {
+input,select{
     display: block;
     width: 100%;
     padding: 8px;
 }
-
+#preview {
+    padding: 10px 20px;
+    border: 1px dotted black;
+    margin: 20px 0;
+}
 h3 {
     margin-top: 10px;
 }
@@ -117,6 +109,5 @@ h3 {
   text-align: center;
   padding: 10px 40px;
 }
-
 
 </style>
--- src/components/userList.vue
+++ src/components/userList.vue
@@ -1,20 +1,21 @@
 <template>
-    <div id="userList">
+    <div>
         <AddUser v-if="mode=='add'" @userStored="addUser($event)" @addClose="openAdd($event)" ></AddUser>
-        <div v-if="mode=='list'">
-            <h3 id="header">User List</h3>
-            <table>
-            <thead>
+        <EditUser v-if="mode=='edit'" @userEdited="editUser($event)" @addClose="openAdd($event)"></EditUser>
+        <div class="user-list" v-if="mode=='list'">
+            <h1 class="header">User List</h1>
+            <table class="table_layout">
+            <thead >
                 <tr>
                 <th>SNo.</th>
                 <th>First Name</th>
                 <th>Last Name</th>
-                <th>Age</th>
+                <th>Age  </th>
                 <th>Gender</th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="(user,index) in users" :key="index">
+                <tr class="active-row" v-for="(user,index) in users" :key="index">
                 <th>{{index+1}}</th>
                 <td>{{user.fname}}</td>
                 <td>{{user.lname}}</td>
@@ -22,15 +23,15 @@
                 <td>{{user.gender}}</td>
                 <td>
                     <form>
-                    <button>Edit</button>
-                    <button>Delete</button>
+                    <button type="button" @click.prevent="openEdit('edit',index)" class="c-button  c-button--edit">Edit</button>
+                    <button @click.prevent="deleteUser(index)" class="c-button  c-button--delete">Delete</button>
                     </form>
                 </td>
                 </tr>
             </tbody>
             </table>
-             <div id="addUser">
-                <button @click="openAdd('add')">Add User</button>
+             <div class="addUser">
+                <button @click="openAdd('add')" class="c-button  c-button--add">Add User</button>
             </div>
         </div>
         
@@ -42,16 +43,20 @@
 
 <script>
 import AddUser from "./addUser.vue";
+import EditUser from "./editUser.vue";
 export default {
     components: {
-        AddUser
+        AddUser,
+        EditUser
     },
     //props:['newUser'],
     data() {
         return {
             mode:'list',
+            editId:'',
             users: [
             {
+                id:Math.floor(Math.random() * 100),
                 fname:'Sam',
                 lname:'John',
                 age:'25',
@@ -74,13 +79,20 @@ export default {
         addUser(user) {
             
             this.mode='list'
-            this.users.push({
-                fname:user.fname,
-                lname:user.lname,
-                age:user.age,
-                gender:user.gender
-            })
-            
+            this.users.push(user)
+
+        },
+        deleteUser(index){
+            this.users.splice(index,1)
+        },
+        openEdit(val,index) {
+            this.mode=val
+            this.editId=index
+            console.log("index is"+index)
+        },
+        editUser(user) {
+            this.$set(this.users,this.editId,user)
+            this.editId=''
         }
         
     },   
@@ -93,5 +105,57 @@ export default {
 
 
 <style>
+
+
+.header {
+    text-align: center;
+    text-transform:uppercase;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    font-weight: 900;
+
+}
+.addUser {
+    text-align: center;
+}
+.c-button {
+  text-align: center;
+  padding: 2px 4px;
+  margin: 5px 10px;
+}
+.c-button--edit {
+    color:white;
+    background-color: rgb(35, 101, 224);
+}
+.c-button--delete {
+    color: white;
+    background-color: red;
+}
+.c-button--add {
+    padding: 8px;
+    margin-top: 20px;
+}
+.table_layout {
+    border-collapse: collapse;
+    margin-left: auto;
+    margin-right: auto;
+    font-size: 0.9em;
+    font-family: sans-serif;
+    min-width: 400px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+    text-align:center;
+}
+
+.table_layout tbody tr:nth-of-type(even) {
+    background-color: #ebe7e7;
+}
+
+.table_layout th, .table_layout td {
+    padding: 12px 15px;
+}
+.table_layout tbody tr {
+    border-bottom: 1px solid #dddddd;
+}
+
+
 
 </style>
